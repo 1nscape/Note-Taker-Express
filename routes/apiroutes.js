abc7a2009890ae0a25ca8464e518e053f5a77779
Max <max.belenchuk@gmail.com>
2023-04-10 19:27:02 -0300

    apiroutes.js complete. GET and POST commands finalized. Also new ID created for each of the new notes, by adding +1. Could have used uuid package instead, if error occurs will do so.

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -4,3 +4,21 @@ const app = require("express").Router();
 var dataNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8")); 
 
 
+app.get("/notes", (req, res) => {
+    res.json(dataNotes);
+});
+
+
+app.post("/notes", (req, res) =>  {
+    let newestNote = req.body; 
+    let noteId = dataNotes.length;
+
+    newestNote.id = noteId + 1; 
+
+    dataNotes.push(newestNote); 
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(dataNotes))
+
+    res.json(dataNotes);
+
+});
